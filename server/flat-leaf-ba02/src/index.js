3abfdfb6f1f334cb78cad03f2bba16eaa1fc779e
rua-iri <rory.mcguigan@protonmail.com>
2024-05-07 03:50:22 +0300

    fix: cors error

--- server/flat-leaf-ba02/src/index.js
+++ server/flat-leaf-ba02/src/index.js
@@ -5,22 +5,46 @@ export default {
 
 		const { pathname } = new URL(request.url);
 
+		const responseHeaders = {
+			"Access-Control-Allow-Headers": "*",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+			"Access-Control-Max-Age": "86400",
+		}
+
 		if (pathname === "/api/students1") {
 			const { results } = await env.DB.prepare(
 				"SELECT * FROM students WHERE id < 7"
 			)
 				.bind()
 				.all();
-			return Response.json(results);
+
+			return Response.json(results,
+				{
+					status: 200,
+					headers: responseHeaders
+				});
+
 		} else if (pathname === "/api/students2") {
 			const { results } = await env.DB.prepare(
 				"SELECT * FROM students WHERE id > 4"
 			)
 				.bind()
 				.all();
-			return Response.json(results);
+
+			return Response.json(results,
+				{
+					status: 200,
+					headers: responseHeaders
+				});
 		}
 
-		return new Response('Invalid Endpoint');
+		return new Response(
+			"Error: invalid endpoint",
+			{
+				status: 400,
+				headers: responseHeaders
+			}
+		);
 	},
 };
